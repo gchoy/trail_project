656c506cdbf8f34dd286ef771fb31ab34e725fb5
gchoy <gabriela.choy@gmail.com>
2020-06-04 15:06:38 -0700

    Added models to admin, created .gitignore file, modified Make file

--- trail_app/admin.py
+++ trail_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Location, Spot, Trail
+
+class LocationAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Location, LocationAdmin)
+
+class SpotAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Spot, SpotAdmin)
+
+class TrailAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Trail, TrailAdmin)
