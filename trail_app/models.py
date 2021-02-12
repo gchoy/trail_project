5ab1bcdf801de391f077cf32c758ccf9f3a15eab
gchoy <gabriela.choy@gmail.com>
2021-02-11 19:46:59 -0800

    added new model: group trail, reversed some modifications, kept many to many field in Trail model

--- trail_app/models.py
+++ trail_app/models.py
@@ -13,9 +13,23 @@ class Location(models.Model):
         return self.name
 
 
+class Spot(models.Model):
+    spot_location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)
+    spot_name = models.CharField(max_length=250)
+    spot_public = models.BooleanField()
+    spot_lon = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+    spot_lat = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+    spot_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.spot_name
+
+
+
 class Trail(models.Model):
     trail_location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)
     trail_name = models.CharField(max_length=250)
+    trail_spots = models.ManyToManyField(Spot, blank=True, null=True)
     trail_public = models.BooleanField()
     trail_lon = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
     trail_lat = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
@@ -29,21 +43,12 @@ class Trail(models.Model):
         lst = [x[1] for x in self.tags.values_list()]
         return str(lst.join(','))"""
 
-class Spot(models.Model):
-    spot_location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)
-    spot_on_trail = models.ForeignKey(Trail, on_delete=models.CASCADE, blank=True, null=True)
-    spot_name = models.CharField(max_length=250)
-    spot_public = models.BooleanField()
-    spot_lon = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
-    spot_lat = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
-    spot_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.spot_name
 
         
 class Trail_Group(models.Model):
     tg_name=models.CharField(max_length=250)
+    tg_trail = models.ForeignKey(Trail, on_delete=models.CASCADE, blank=True, null=True)
     tg_start = models.OneToOneField(Location, unique=True,on_delete=models.CASCADE, blank=True, null=True,related_name='tg_start')
     
     tg_start_lon = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
